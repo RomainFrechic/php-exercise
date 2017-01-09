86d9749463f34dfeb1903364bf170209373eb5c1
Romain <RomainFrechic@outlook.fr>
2017-01-09 13:43:28 +0100

    function exercice 6

---
+++ function/exercice6/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function exercice 6</title>
+</head>
+<body>
+	<?php 
+ 			function profil(){
+ 				$nom = "Maximilien ";
+ 				$prenom = "Simplon";
+ 				$age = 30;
+
+ 				echo "Bonjour $nom $prenom ,tu as $age ans.";
+
+ 			}
+ 			profil();
+	?>
+</body>
+</html>
