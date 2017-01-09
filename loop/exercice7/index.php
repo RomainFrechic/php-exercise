03094d854bbc5442b7efc844a4b151ffb5596ec8
Romain <RomainFrechic@outlook.fr>
2017-01-09 11:28:59 +0100

    loop exercice 7

---
+++ loop/exercice7/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop exercice 7</title>
+</head>
+<body>
+	<?php
+		$A = 100;
+		for($A=0; $A <= 100; $A=$A+15){
+			
+			echo "On tient le bon bout.";
+		}
+	?>
+</body>
+</html>
