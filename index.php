dbfacb53e851f19bee76dc0e1ee47c9f12f673ed
Romain <RomainFrechic@outlook.fr>
2017-01-09 09:30:27 +0100

    loop exercice 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop Exercice 1</title>
+</head>
+<body>
+	<?php  
+		$A = 0;
+		for ($A = 0; $A <= 10; $A++){
+			echo $A;
+		}
+	?> 
+</body>
+</html>
