be7a5321e63429965057f50b1c6683ee5a305a88
Romain <RomainFrechic@outlook.fr>
2017-01-09 11:34:17 +0100

    loop exercice 8

---
+++ loop/exercice8/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop Exercice 8</title>
+</head>
+<body>
+	<?php
+		$A =200;
+		for($A=200; $A>=0; $A=$A-12){
+			echo "Enfin !!!!";
+		}
+
+	?>
+</body>
+</html>
