634a22af99ad9f706d76203c4de0895964802d0d
Romain <RomainFrechic@outlook.fr>
2017-01-09 11:53:55 +0100

    function exercice 1

---
+++ function/exercice1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function exercice 1</title>
+</head>
+<body>
+	<?php
+
+		function veriter(){
+			$A =true;
+             return $A;
+		}
+		echo veriter();
+	?>
+</body>
+</html>
