9263b6b5e4fa3e7e393ee46693c7d29ae6f25ed1
Romain <RomainFrechic@outlook.fr>
2017-01-09 16:29:36 +0100

    array exercice 5

---
+++ array/exercice5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array exercice 5</title>
+</head>
+<body>
+	<?php
+		$tableau = array(
+"87" => "Nord",
+"02" => "Aisne",
+"60" => "Oise",
+"62" => "Pas de calais",
+"59" => "Nord",
+"80" => "Somme"
+
+			);
+
+		var_dump($tableau);
+	?>
+</body>
+</html>
