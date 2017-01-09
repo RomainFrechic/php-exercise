431c32d8185f9ee3677d7768a0399612787a5a84
Romain <RomainFrechic@outlook.fr>
2017-01-09 11:06:33 +0100

    loop exercice 5

---
+++ loop/exercice5/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop exercice 5</title>
+</head>
+<body>
+	<?php
+	$A =1;
+	for($A=1; $A <= 15; $A++){
+		
+		echo "On y arrive presque.";
+
+	}
+	?>
+</body>
+</html>
