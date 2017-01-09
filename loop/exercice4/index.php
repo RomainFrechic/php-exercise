e14d58a509b670856dc1162703729266ed1dc8fc
Romain <RomainFrechic@outlook.fr>
2017-01-09 10:59:07 +0100

    loop exercice 4

---
+++ loop/exercice4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop exercice 4</title>
+</head>
+<body>
+	<?php
+		$A = 1;
+	for($A = 1; $A <= 10; $A+= $A/2){
+		echo $A;
+	
+	}
+	?>
+</body>
+</html>
