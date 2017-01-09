8c47a6d10c4af5134f3ad39bcb997c5d4d64a6cc
Romain <RomainFrechic@outlook.fr>
2017-01-09 15:45:08 +0100

    function exercice 8

---
+++ function/exercice8/index.php
@@ -0,0 +1,24 @@
+<?php
+		$nbr1 = 5;
+		$nbr2 = 15;
+		$nbr3 = 85;
+
+		function addNombre($nbr1,$nbr2,$nbr3){
+			$nbrResult = $nbr1 + $nbr2 + $nbr3;
+			echo $nbrResult;
+
+		}
+	?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function exercice 8</title>
+</head>
+<body>
+<?php
+    addNombre(5,15,10);
+?>
+</body>
+</html>
