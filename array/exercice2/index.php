868c01ba08ea0fbf6010b7968f79d2292f8b212b
Romain <RomainFrechic@outlook.fr>
2017-01-09 16:00:58 +0100

    array exercice 2

---
+++ array/exercice2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array exercice 2</title>
+</head>
+<body>
+	<?php
+	$array = array('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
+	echo $array['2'];
+
+
+	?>
+</body>
+</html>
