e64c436977d108300aa4f6e77b9d8cc997c74645
Romain <RomainFrechic@outlook.fr>
2017-01-09 11:13:01 +0100

    loop exercice 6

---
+++ loop/exercice6/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loop exercice 5</title>
+</head>
+<body>
+	<?php 
+		$A = 20;
+		for($A = 20; $A >= 0; $A--){
+
+			echo "C'est presque bon";
+		}
+
+	?>
+</body>
+</html>
