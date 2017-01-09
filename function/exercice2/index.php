1513c4d24e3c48dd2e49d1d2e2458b396742804c
Romain <RomainFrechic@outlook.fr>
2017-01-09 12:01:44 +0100

    function exercice 2

---
+++ function/exercice2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function exercice 2</title>
+</head>
+<body>
+	<?php
+
+		function string(){
+			$A ="Je suis une string";
+             return $A;
+		}
+		echo string();
+	?>
+</body>
+</html>
