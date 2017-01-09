b848eed5f21f3794d4527541c8526203c79d8692
Romain <RomainFrechic@outlook.fr>
2017-01-09 15:53:43 +0100

    array exercice 1

---
+++ array/exercice1/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array exercice 1</title>
+</head>
+<body>
+<?php
+$array = array('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
+var_dump($array);
+
+?>
+
+	
+</body>
+</html>
