85d0089e657b69c8224619e21bf796f353fea280
Romain <RomainFrechic@outlook.fr>
2017-01-09 12:02:18 +0100

    function exercice 3

---
+++ function/exercice3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function exercice 3</title>
+</head>
+<body>
+	<?php
+		function stringTwin(){
+			$A ="Je suis une string ";
+			$B ="Je suis la deuxieme string";
+             return $A.$B;
+		}
+		echo stringTwin();
+	?>
+</body>
+</html>
