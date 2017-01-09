023a82d18fa3b7a74a9a2ceef8f50b4fb5ec19e2
Romain <RomainFrechic@outlook.fr>
2017-01-09 12:12:44 +0100

    function exercice 4

---
+++ function/exercice4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function exercice 4</title>
+</head>
+<body>
+	<?php
+	$A = 25;
+	$B = 15;
+	if($A < $B){
+		echo "le nombre $A est plus petit que le nombre $B";
+	}
+	if($A > $B){
+		echo "le nombre $A est grand que le nombre $B";
+	}
+	if($A==$B){
+		echo "les nombre son égaux";
+			}
+	?>
+</body>
+</html>
