91eb607d0701302fc62fff3d1a51c12887b53d63
Romain <RomainFrechic@outlook.fr>
2017-01-09 13:35:36 +0100

    function exercice 5

---
+++ function/exercice5/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Function exercice 5</title>
+</head>
+<body>
+	<?php
+		$A = "Le nombre concatené avec la phrase est ";
+		$B = 100;
+
+		echo $A.$B;
+
+	?>
+	
+</body>
+</html>
