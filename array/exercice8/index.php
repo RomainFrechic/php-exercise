47083fc28f27e2a3748af1b1f90df839afbdc2d0
Romain <RomainFrechic@outlook.fr>
2017-01-10 08:53:59 +0100

    array exercice 8

---
+++ array/exercice8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array exercice 8</title>
+</head>
+<body>
+	<?php
+	$array = array('Janvier ','Février ','Mars ','Avril ','Mai ','Juin ','Juillet ','Août ','Septembre ','Octobre ','Novembre ','Décembre');
+
+	foreach ($array as $key) {
+		echo $key;
+		
+
+	}
+
+	
+	?>
+</body>
+</html>
