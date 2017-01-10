584cd1fc6b0194cefce2ef25dea13d6cc11311c3
Romain <RomainFrechic@outlook.fr>
2017-01-10 12:23:17 +0100

    form exercice 1

---
+++ form/exercice1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form exercice 1</title>
+</head>
+<body>
+	<form action="user.php" method="GET">
+<div>		
+<label for="name">Nom</label>
+<input type="text" value="Rick " name="name" id="name">
+</div>
+<div>		
+<label for="name">Prénom</label>
+<input type="text" value="Hunter" name="name" id="name">
+</div>
+<button>Envoyer</button>
+	</form>
+</body>
+</html>
