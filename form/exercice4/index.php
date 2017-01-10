a2ee49c7e838c427dcb845d9f3bfb4513fe08b1e
Romain <RomainFrechic@outlook.fr>
2017-01-10 13:56:04 +0100

    form exercice 4

---
+++ form/exercice4/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form exercice 4</title>
+</head>
+<body>
+	<form action="user.php" method="POST">
+		
+<label for="name">Nom</label>
+<input type="text" value="John " name="name" id="name">	
+<input type="text" value="Lennon" name="prenom" id="prenom">
+
+<button type="submit">Envoyer</button>
+	</form>
+</body>
+</html>
---
+++ form/exercice4/user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form exercice 4</title>
+</head>
+<body>
+	<?php 
+   foreach ($_POST as $value) {
+   	echo $value;
+}
+	 ?>
+</body>
+</html>
