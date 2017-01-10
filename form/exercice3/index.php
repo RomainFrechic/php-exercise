ee14815893d691ac80f2978f8b3a15836a4e5bff
Romain <RomainFrechic@outlook.fr>
2017-01-10 13:31:22 +0100

    form exercice 3

---
+++ form/exercice3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form exercice 3</title>
+</head>
+<body>
+	<form action="user.php" method="GET">
+		<div>		
+			<label for="name">Nom</label>
+			<input type="text" value=" Charlie " name="name" id="name">
+			<input type="text" value=" Chaplin " name="name" id="name">
+		</div>
+		<button>Envoyer</button>
+	</form>
+</body>
+</html>
---
+++ form/exercice3/user.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form exercice 3</title>
+</head>
+<body>
+	<?php 
+   foreach ($_GET as $value) {
+   	echo $value;
+}
+
+
+	 ?>
+</body>
+</html>
