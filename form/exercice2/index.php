c35cf5884fd7a914bccb4b69773573a4da3e9030
Romain <RomainFrechic@outlook.fr>
2017-01-10 12:27:49 +0100

    form exercice 2

---
+++ form/exercice2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form exercice 2</title>
+</head>
+<body>
+	<form action="user.php" method="POST">
+<div>		
+<label for="name">Nom</label>
+<input type="text" value="John " name="name" id="name">
+</div>
+<div>		
+<label for="name">Prénom</label>
+<input type="text" value="Lennon" name="name" id="name">
+</div>
+<button>Envoyer</button>
+	</form>
+</body>
+</html>
---
+++ form/exercice2/user.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form exercice 2</title>
+</head>
+<body>
+	<h1>Bravo ! Vous êtes sur la page USER.</h1>
+</body>
+</html>
