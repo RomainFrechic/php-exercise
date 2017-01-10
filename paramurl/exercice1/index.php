c7a2fbafcc10396d5c375198f36fd294977429ee
Romain <RomainFrechic@outlook.fr>
2017-01-10 09:31:09 +0100

    paramurl exercice 1

---
+++ paramurl/exercice1/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>ParamUrl exercice 1</title>
+</head>
+<body>
+	<a href="index.php?nom=Nemare&amp;prenom=Jean">Liens</a>
+</body>
+</html>
