eb7e96de618320e6b897434f85d1a76ecb2840cb
Romain <RomainFrechic@outlook.fr>
2017-01-10 14:30:22 +0100

    form exercice 6

---
+++ form/exercice6/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="./style.css">
+	<title>Form exercice 6</title>
+</head>
+<body>
+<?php 
+  if(empty($_POST)){
+ ?>
+	<form action="index.php" method="POST">
+		
+		<label for="name">Nom</label>
+		<select name="Genre">
+			<option value="Mr ">Mr</option>
+			<option value="Mme ">Mme</option></select>
+			<input type="text" value="John " name="name" id="name">	
+			<input type="text" value="Lennon" name="prenom" id="prenom">
+
+			<button type="submit">Envoyer</button>
+		</form>
+
+<?php 
+}else{
+
+   foreach ($_POST as $value) {
+   	echo $value;
+}
+	
+}
+
+?>
+
+	</body>
+	</html>
