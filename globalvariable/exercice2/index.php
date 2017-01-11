f00517c81a5b958b1efec7f02be41887e30e503c
Romain <RomainFrechic@outlook.fr>
2017-01-11 09:32:02 +0100

    global variable exercice 2

---
+++ globalvariable/exercice2/index.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+
+$_SESSION['nom'] = 'Chaplin ';
+$_SESSION['prenom'] = 'Charlie ';
+$_SESSION['age'] = 60;
+
+
+ ?>
+<!DOCTYPE html>
+<meta charset="UTF-8">
+
+<title>GlobalVariable exercice 2</title>
+</head>
+<body>
+	<a href="user.php">Liens user.php</a>
+			
+		
+	</body>
+	</html>
