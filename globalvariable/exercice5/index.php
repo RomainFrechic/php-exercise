83b9450b38d46d4c203ff034830fdf0a301b233e
Romain <RomainFrechic@outlook.fr>
2017-01-11 11:04:57 +0100

    global variable exercice 5

---
+++ globalvariable/exercice5/index.php
@@ -0,0 +1,28 @@
+<?php 
+setcookie('login',$_POST['login'] ,time()+ 365*24*3600, null, null, false, true);
+setcookie('pwd',$_POST['pwd'] ,time()+ 365*24*3600, null, null, false, true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+
+<title>GlobalVariable exercice 5</title>
+</head>
+<body>
+	<form action="index.php" method="POST">
+
+		Votre login : <input type="text" name="login" value="romain ">
+		<br />
+		Votre mot de passé : <input type="password" name="pwd" value="frechic "><br />
+		<input type="submit" value="Connexion">
+	</form>
+	<a href="user.php">Liens user.php</a></br>
+
+
+	<?php 
+	echo "le login est : ".$_COOKIE['login'].'</br>';
+	echo "le Mot de passe est : ".$_COOKIE['pwd'];
+	?>
+
+</body>
+</html>
---
+++ globalvariable/exercice5/user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>GlobalVariable exercice 5</title>
+</head>
+<body>
+	<?php 
+
+	echo "le login est : ".$_COOKIE['login'].'</br>';
+	echo "le Mot de passe est : ".$_COOKIE['pwd'];?>
+
+</body>
+</html>
