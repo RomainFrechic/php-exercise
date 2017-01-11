8d0e7bcf26a30f0eb9a9361f3bd8444744dbdb7d
Romain <RomainFrechic@outlook.fr>
2017-01-11 10:18:33 +0100

    global variable exercice 4

---
+++ globalvariable/exercice4/user.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>GlobalVariable exercice 4</title>
+</head>
+<body>
+	<?php 
+
+	echo "le login est : ".$_COOKIE['login'].'</br>';
+	echo "le Mot de passe est : ".$_COOKIE['pwd'];?>
+
+</body>
+</html>
