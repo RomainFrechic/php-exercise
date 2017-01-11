b48c3f226e45603bd93ea0f111fdcadfc5f13b9c
Romain <RomainFrechic@outlook.fr>
2017-01-11 14:51:23 +0100

    date exercice 7

---
+++ date/exercice7/index.php
@@ -0,0 +1,14 @@
+<?php 
+$maDate = date('Y/m/d', strtotime('+20 day')); 
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+
+<title>Date exercice 7</title>
+</head>
+<body>
+
+  <h1>La date du jour + 20 jours :<?php echo $maDate; ?></h1>
+</body>
+</html>
