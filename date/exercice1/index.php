6413b808881f3874f23ce2cce690701eba95356e
Romain <RomainFrechic@outlook.fr>
2017-01-11 11:43:11 +0100

    date exercice 1

---
+++ date/exercice1/index.php
@@ -0,0 +1,14 @@
+<?php 
+$jour = date("d/m/y");	
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+
+<title>Date 1</title>
+</head>
+<body>
+
+<h1>La date du Jour est <?php echo $jour; ?></h1>
+</body>
+</html>
