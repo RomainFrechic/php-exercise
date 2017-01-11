096713d794da122f4e9c83b84fe4691ec980a38f
Romain <RomainFrechic@outlook.fr>
2017-01-11 14:51:57 +0100

    date exercice 8

---
+++ date/exercice8/index.php
@@ -0,0 +1,14 @@
+<?php 
+$maDate = date('Y/m/d', strtotime('-22 day')); 
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+
+<title>Date exercice 8</title>
+</head>
+<body>
+
+  <h1>La date du jour - 22 jours :<?php echo $maDate; ?></h1>
+</body>
+</html>
